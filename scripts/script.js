b94811efd93c4ceac3ad6d6977abad5a6149eda0
i-am-bot003 <82062758+i-am-bot003@users.noreply.github.com>
2021-05-28 01:00:28 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,118 @@
+
+function down(numer){
+    let a = document.getElementById("ping1")
+    if (numer = 1){
+        a.style.visibility = 'visible';
+    }
+}
+
+function up(numer){
+    let a = document.getElementById("ping1")
+    if(numer=1){
+        a.style.visibility = 'hidden';
+    }
+}
+
+
+function down1(numer){
+    let a = document.getElementById("ping2")
+    if (numer = 1){
+        a.style.visibility = 'visible';
+    }
+}
+
+function up1(numer){
+    let a = document.getElementById("ping2")
+    if(numer=1){
+        a.style.visibility = 'hidden';
+    }
+}
+
+
+function down2(numer){
+    let a = document.getElementById("ping3")
+    if (numer = 1){
+        a.style.visibility = 'visible';
+    }
+}
+
+function up2(numer){
+    let a = document.getElementById("ping3")
+    if(numer=1){
+        a.style.visibility = 'hidden';
+    }
+}
+
+
+function down3(numer){
+    let a = document.getElementById("ping4")
+    if (numer = 1){
+        a.style.visibility = 'visible';
+    }
+}
+
+function up3(numer){
+    let a = document.getElementById("ping4")
+    if(numer=1){
+        a.style.visibility = 'hidden';
+    }
+}
+
+
+function down4(numer){
+    let a = document.getElementById("ping5")
+    if (numer = 1){
+        a.style.visibility = 'visible';
+    }
+}
+
+function up4(numer){
+    let a = document.getElementById("ping5")
+    if(numer=1){
+        a.style.visibility = 'hidden';
+    }
+}
+
+
+function down5(numer){
+    let a = document.getElementById("ping6")
+    if (numer = 1){
+        a.style.visibility = 'visible';
+    }
+}
+
+function up5(numer){
+    let a = document.getElementById("ping6")
+    if(numer=1){
+        a.style.visibility = 'hidden';
+    }
+}
+
+function down6(numer){
+    let a = document.getElementById("ping7")
+    if (numer = 1){
+        a.style.visibility = 'visible';
+    }
+    
+}
+
+function up6(numer){
+    let a = document.getElementById("ping7")
+    if(numer=1){
+        a.style.visibility = 'hidden';
+    }
+}
+
+function down7(numer){
+    let a = document.getElementById("ping8")
+    if (numer = 1){
+        a.style.visibility = 'visible';
+    }
+}
+
+function up7(numer){
+    let a = document.getElementById("ping8")
+    if(numer=1){
+        a.style.visibility = 'hidden';
+    }
+}
